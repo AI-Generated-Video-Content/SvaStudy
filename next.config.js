1bcf446fddf9a3373db0c27dbce38fcec8df6f90
Ayush Yadav <ayushyadav4964@gmail.com>
2025-03-26 21:01:58 +0530

    update

--- next.config.js
+++ next.config.js
@@ -1,8 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // This exports your app as static HTML
+  basePath: process.env.NODE_ENV === 'production' ? '/SvaStudy' : '',  // Use your GitHub repo name
   images: {
-    domains: ['placehold.co'],
+    unoptimized: true,  // Required for static export
   },
-}
+};
 
-module.exports = nextConfig 
+module.exports = nextConfig; 
